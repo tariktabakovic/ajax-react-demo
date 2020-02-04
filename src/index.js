e533c293eac452e1d7b7fa3f635259b2385538bf
tariktabakovic <tariktab97@gmail.com>
2020-02-04 13:32:54 -0500

    imports axios and made sure data is received from the API

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import axios from 'axios';
+const API_ENDPOINT = 'https://swapi.co/api/people/1/';
+
+axios.get(API_ENDPOINT)
+    .then(response=>{
+        console.log(response);
+    })
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
